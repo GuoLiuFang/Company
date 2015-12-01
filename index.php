3f8d3a243e971a97f5015cf0ddd5bd1437905c68
LiuFangGuo <guoliufang@tigerjoys.com>
2015-12-01 16:17:58 +0800

    公司主页草创

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>FunComing</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicons
+        ================================================== -->
+    <link rel="apple-touch-icon" href="Images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="Images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="Images/apple-touch-icon-114x114.png">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css"  href="Stylesheets/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome/Stylesheets/font-awesome.css">
+    <!-- Slider
+        ================================================== -->
+    <link href="Stylesheets/owl.carousel.css" rel="stylesheet" media="screen">
+    <link href="Stylesheets/owl.theme.css" rel="stylesheet" media="screen">
+    <!-- Stylesheet
+        ================================================== -->
+    <link rel="stylesheet" type="text/css"  href="Stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="Stylesheets/prettyPhoto.css">
+    <link href='Stylesheets/lato.css' rel='stylesheet' type='text/css'>
+    <link href='Stylesheets/openSans.css' rel='stylesheet' type='text/css'>
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- Navigation -->
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header" style="padding-top:10px;">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <img src="Images/xx.png" height="24" width="76px">
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav" style="font-weight:bold;font-family:Microsoft Yahei;">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="index.html">首页</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="service.html">我们的服务</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="culture.html">企业文化</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="joinus.html">加入我们</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Header -->
+<header class="intro" style="background-image:url(Images/banner/bg-img.png); background-position:bottom; background-repeat:no-repeat;">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2" style="font-family:Microsoft Yahei;">
+                    <h1>We are <sapn style="color: #c10650">F</sapn><sapn style="color: #3e9f07">u</sapn><sapn style="color: #fe5850">n</sapn><sapn style="color: #fe5850">C</sapn><sapn style="color: #ffbb07">o</sapn><sapn style="color: #0aafbd">m</sapn><sapn style="color: #fe5850">i</sapn><sapn style="color: #3e9f07">n</sapn><sapn style="color: #2f6f85">g</sapn> </h1>
+                    <p class="intro-text" style="font-family:Microsoft Yahei;">专注于无线互联网领域，拥有核心能力，面向市场的运营型公司</p>
+                    <a href="#services" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- Services Section -->
+<div id="services" class="text-center">
+    <div class="container">
+        <div class="section-title center">
+            <h2><strong>我们的服务</strong></h2>
+            <hr>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 service"> <i class="fa fa-jpy"></i>
+                <h4><strong><a href = "service.html">小额支付</a></strong></h4>
+                <p>通过支付SDK为核心合作伙伴提供小额支付解决方案</p>
+            </div>
+            <div class="col-md-4 col-sm-6 service"> <i class="fa fa-users"></i>
+                <h4><strong><a href = "service.html">用户平台</a></strong></h4>
+                <p>通过支付SDK及广告SDK为核心合作伙伴提供全方位的支撑服务</p>
+            </div>
+            <div class="col-md-4 col-sm-6 service">
+                <i class="fa fa-database"></i>
+                <h4><strong><a href = "service.html">大数据挖掘</a></strong></h4>
+                <p>深入挖掘数据价值产生需求合作，同时提供精准的数据营销服务创造更大的价值链</p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Section -->
+<div id="works">
+    <div class="container"> <!-- Container -->
+        <div class="section-title text-center center">
+            <h2><strong>企业文化</strong></h2>
+            <hr>
+        </div>
+        <div class="categories">
+            <ul class="cat">
+                <li>
+                    <ol class="type">
+                        <li><a href="#" data-filter="*" class="active">All</a></li>
+                        <li><a href="#" data-filter=".web">办公环境</a></li>
+                        <li><a href="#" data-filter=".app">茶余时刻</a></li>
+                        <li><a href="#" data-filter=".branding">体育活动</a></li>
+                    </ol>
+                </li>
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+        <div class="row">
+            <div class="portfolio-items">
+                <div class="col-sm-6 col-md-3 col-lg-3 web">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/14.png" title="头脑风暴，手绘高手" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>设计部</h4>
+                                    <small>头脑风暴，手绘高手</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/14.png" class="img-responsive" alt="设计部"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 web">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/1.png" title="保持进度，追求品质" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>开发部</h4>
+                                    <small>保持进度，追求品质</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/1.png" class="img-responsive" alt="开发部"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 app">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/10.png" title="一杯清茶，一本杂志，一颗平凡之心" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>休息室</h4>
+                                    <small>一杯清茶，一本杂志，一颗平凡之心</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/10.png" class="img-responsive" alt="休息室"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 web">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/11.png" title="机械键盘，HIFI耳机，我是时尚达人" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>事业部</h4>
+                                    <small>机械键盘，HIFI耳机，我是时尚达人</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/11.png" class="img-responsive" alt="事业部"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 web">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/12.png" title="站如松，坐如钟，行如风，我就是程序猿" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>开发部</h4>
+                                    <small>站如松，坐如钟，行如风，我就是程序猿</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/12.png" class="img-responsive" alt="开发部"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 branding">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/7.png" title="我是屌丝，我更是运动健儿" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>羽毛球馆</h4>
+                                    <small>我是屌丝，我更是运动健儿</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/7.png" class="img-responsive" alt="羽毛球馆"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 branding branding">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/3.png" title="场下，我们是同事，场上，我们是兄弟" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>篮球馆</h4>
+                                    <small>场下，我们是同事，场上，我们是兄弟</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/3.png" class="img-responsive" alt="篮球馆"> </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 app">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="Images/portfolio/13.png" title="巧妙部署，奋力厮杀，斗智斗勇" rel="prettyPhoto">
+                                <div class="hover-text">
+                                    <h4>休息室</h4>
+                                    <small>巧妙部署，奋力厮杀，斗智斗勇</small>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-search"></i> </div>
+                                <img src="Images/portfolio/13.png" class="img-responsive" alt="休息室"> </a> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About Section -->
+<div id="about">
+    <div class="container">
+        <div class="section-title text-center center">
+            <h2><strong>关于我们</strong></h2>
+            <hr>
+        </div>
+        <div class="row">
+            <div class="col-md-6"> <img src="Images/about.png" class="img-responsive"> </div>
+            <div class="col-md-6">
+                <div class="about-text"> <i class="fa fa-users"></i>
+                    <div class="padding-left"><h3>Who We Are</h3>
+                        <p style="font-size:16px;">老虎致远是一家专注于无线互联网领域，拥有核心能力，面向市场，深耕产业提炼价值的运营型公司。</p></div>
+                    <i class="fa fa-check-square-o"></i>
+                    <div class="padding-left"><h3>Why Choose Us</h3>
+                        <p style="font-size:16px;">核心运营思想，核心技术，闭环控制，数据采集、精准分析、产生需求、创造价值。</p></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="achivements" class="section dark-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-3">
+                <div class="achivement-box">
+                    <i class="fa fa-smile-o" style="display:block;" ></i>
+                    <span class="count" style="display:inline-block;">500</span>
+                    <span style="font-size:20px; font-weight:700; color:#fff;">+合作伙伴</span>
+                    <h4>为他们提供服务</h4>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-3">
+                <div class="achivement-box">
+                    <i class="fa fa-usd" style="display:block;"></i>
+                    <span class="count" style="display:inline-block;">30,000,000</span>
+                    <span style="font-size:20px; font-weight:700; color:#fff;">+结算金额</span>
+                    <h4>每年为合作伙伴结算金额</h4>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-3">
+                <div class="achivement-box">
+                    <i class="fa fa-users" style="display:block;"></i>
+                    <span class="count" style="display:inline-block;">150,000,000</span>
+                    <span style="font-size:20px; font-weight:700; color:#fff;">+覆盖用户</span>
+                    <h4>用户平台拥有海量用户</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Contact Section -->
+<div id="contact" class="text-center">
+    <div class="container">
+        <div class="section-title center">
+            <h2><strong>联系我们</strong></h2>
+            <hr style="margin-bottom:20px;">
+            <p>我们期待每一个和你交流的机会，无论是投诉还是祝福，意见或建议，想知道老虎致远最近做了什么，还是想加入我们的团队，这里就是开始。</p>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-4"> <i class="fa fa-map-marker fa-2x"></i>
+                <p>北京市海淀区中关村大街1号<br>
+                    12层1205</p>
+            </div>
+            <div class="col-md-4"> <i class="fa fa-envelope-o fa-2x"></i>
+                <p>business@tigerjoys.com</p>
+            </div>
+            <div class="col-md-4"> <i class="fa fa-phone fa-2x"></i>
+                <p> 010 - 82660219</p>
+            </div>
+            <hr>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+</div>
+<nav id="footer">
+    <div class="container">
+        <div class="fnav" >
+            <p style="text-align:center;">© 2013-2015 北京老虎致远科技有限公司 , All Rights Reserved. 京ICP备案13016343号-1</p>
+        </div>
+    </div>
+</nav>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.js"></script>
+<script type="text/javascript" src="js/jquery.counterup.js"></script>
+<script type="text/javascript" src="js/waypoints.js"></script>
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+<script type="text/javascript" src="js/contact_me.js"></script>
+<script src="js/owl.carousel.js"></script>
+
+<!-- Javascripts
+    ================================================== -->
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
